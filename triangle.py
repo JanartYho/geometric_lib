6a88bc332ec5b695a9d9677662501b98a30d1a8c
Kirill Lukashenko <466542@niuitmo.ru>
2024-11-22 15:32:00 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,9 +1,15 @@
 def area(a, b, c):
-    '''Функция принимает значения - стороны треугольника'''
-    return (a + b + c) / 2
-    '''Функция возвращает (a + b + c) / 2'''
+    if a < 0 or b < 0 or c < 0:
+        raise ValueError("Sides cannot be negative")
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise ValueError("Invalid triangle sides")
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
 
 def perimeter(a, b, c):
-    '''Функция принимает значения - стороны треугольника'''
+    if a < 0 or b < 0 or c < 0:
+        raise ValueError("Sides cannot be negative")
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise ValueError("Invalid triangle sides")
     return a + b + c
-    '''Функция возвращает периметр треугольника'''
