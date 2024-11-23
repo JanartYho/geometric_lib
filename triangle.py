ba83ca3e57bd5ab3a00844625251ad72588182d9
Kirill Lukashenko <466542@niuitmo.ru>
2024-11-23 18:30:51 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,8 +1,10 @@
 def area(a, b, c):
     if a < 0 or b < 0 or c < 0:
         raise ValueError("Sides cannot be negative")
-    if a + b <= c or a + c <= b or b + c <= a:
+
+    if not (a + b > c and a + c > b and b + c > a):
         raise ValueError("Invalid triangle sides")
+
     p = (a + b + c) / 2
     return (p * (p - a) * (p - b) * (p - c)) ** 0.5
 
@@ -10,6 +12,8 @@ def area(a, b, c):
 def perimeter(a, b, c):
     if a < 0 or b < 0 or c < 0:
         raise ValueError("Sides cannot be negative")
-    if a + b <= c or a + c <= b or b + c <= a:
+
+    if not (a + b > c and a + c > b and b + c > a):
         raise ValueError("Invalid triangle sides")
+
     return a + b + c
